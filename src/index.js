b468d7b330c1028b7f9b3bd05c0a891a777c08ba
zfechter88 <99442701+zfechter88@users.noreply.github.com>
2022-02-22 11:44:25 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import React from "react";
 
-
+import App from "App.js";
 
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
   <StrictMode> 
   <p>Hello world.</p>,
+  <App />,
   </StrictMode>,
   rootElement
 );
