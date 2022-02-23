3e17a11cd4f034c24bea83c60476669e08939f1e
zfechter88 <99442701+zfechter88@users.noreply.github.com>
2022-02-23 10:05:30 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-import "./styles.css";
+import "styles.css";
 
 const rootElement = document.getElementById("root");
 
