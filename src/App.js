52f8a4bd1613bce3c7a3a9c724333332027f2436
zfechter88 <99442701+zfechter88@users.noreply.github.com>
2022-02-23 10:11:11 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./styles.css";
 import { useEffect } from "react";
 import { useState } from "react";
-import "styles.css";
 
 function PlayGameContainer() {
   //score
